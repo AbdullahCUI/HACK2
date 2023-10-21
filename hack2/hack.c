ddb261ddc31e6f3cff1da50f77df587d41e744c1
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-22 00:28:15 +0500

    use of output and input function's

--- hack2/hack.c
+++ hack2/hack.c
@@ -11,9 +11,23 @@ double degree_to_radian=(double value);
 int main(){
 
 
-double longitude1,PA1,longitude2,PA2;
+double laditude1,longitude1,laditude2,longitude2;
 
 double distance=0.0, delta= 0.0;
 
 const int radius =6371;
+printf("Enter the latitude_01 in Degree 90 to _90");
+
+scanf("%lf", & laditude1);
+
+printf("Enter langitude_01 degree 180 to -180");
+scanf("%lf", &longitude1);
+
+printf("Enter latitude_02 degree is 90 to -90");
+
+scanf("lf",& laditude2);
+
+printf("Enter longitude_02  Degree=> 180 to -180");
+
+scanf("lf", & longitude2);
 }
