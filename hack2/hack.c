53f340f39513cee1afc652de527b2a4b3051f044
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-22 01:12:15 +0500

    last change

--- hack2/hack.c
+++ hack2/hack.c
@@ -7,6 +7,7 @@
 
 #include<stdlib.h>
 
+#include<math.h>
 double degree_to_radian(double value);
 int main(){
 
@@ -16,20 +17,20 @@ double laditude1,longitude1,laditude2,longitude2;
 double distance=0.0, delta= 0.0;
 
 const int radius =6371;
-printf("Enter the latitude_01 in Degree 90 to _90");
+printf("Enter the latitude_01 in Degree 90 to _90\n");
 
+scanf("%lf", & longitude1);
+
+printf("Enter langitude_01 degree 180 to -180\n");
 scanf("%lf", & laditude1);
 
-printf("Enter langitude_01 degree 180 to -180");
-scanf("%lf", &longitude1);
+printf("Enter latitude_02 degree is 90 to -90\n");
 
-printf("Enter latitude_02 degree is 90 to -90");
+scanf("lf",& longitude2);
 
-scanf("lf",& laditude2);
+printf("Enter longitude_02  Degree=> 180 to -180\n");
 
-printf("Enter longitude_02  Degree=> 180 to -180");
-
-scanf("lf", & longitude2);
+scanf("lf", & laditude2);
 
 laditude1=degree_to_radian(laditude1);
 
@@ -42,15 +43,15 @@ longitude2=degree_to_radian(longitude2);
 
 delta=laditude2 - laditude1;
 
-distance=(acos(sin(longitude1) *sin (longitude2)+cos(longitude2) * cos(delta) * radius));
+distance=(acos(sin(longitude1)* sin (longitude2) + cos (longitude2) * cos (longitude2) * cos (delta)) * radius);
 
 printf("<=Location Distance =>\n");
 
 printf("====================\n");
 
-printf("origin = %lf \n", longitude1,laditude1);
+printf("origin = %lf \n", laditude1,longitude1);
 
-printf("Distination = %lf ", longitude2, laditude2);
+printf("Distination = %.2lf \n", laditude2, longitude2);
  
  printf("Distance= %lf \n ", distance);
 
