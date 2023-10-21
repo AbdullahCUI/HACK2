4dbcce259c86d2fc1738c783bd845e72b5c13cac
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-22 00:13:25 +0500

    hack2 variable declared

---
+++ hack2/hack.c
@@ -0,0 +1,19 @@
+/**
+ * Name:Abdullah
+ * Section:2D
+ *  
+ * **/
+#include<stdio.h>
+
+#include<stdlib.h>
+
+double degree_to_radian=(double value);
+int main(){
+
+
+double longitude1,PA1,longitude2,PA2;
+
+double distance=0.0, delta= 0.0;
+
+const int radius =6371;
+}
